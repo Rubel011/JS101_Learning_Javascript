0bfeba18bf712763d232b45e3e9b4c6cd221c809
Rubel Foridi <rubelforidi@gmail.com>
2022-10-12 01:38:50 +0530

    day3 update

---
+++ Day3/problem4.js
@@ -0,0 +1,13 @@
+// Problem 4: Given 3 numbers (all different values), print which is greatest
+let a = 10;
+let b = 20;
+let c = 30;
+if (a > b && a > c) {
+  console.log(a, "is Greater");
+}
+else if (b > a && b > c) {
+  console.log(b, "is Greater");
+}
+else if (c > b && c > a) {
+  console.log(c, "is Greater");
+}
---
+++ Day3/problem5.js
@@ -0,0 +1,18 @@
+//Problem 5: Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday", ...
+let day='Wed';
+switch(day){
+  case "Sun":console.log("Sunday");
+    break;
+  case "Mon":console.log("Monday");
+    break;
+  case "Tue":console.log("Tuesday");
+    break;
+  case "Wed":console.log("Wednesday");
+    break;
+  case "Thu":console.log("Thusday");
+    break;
+  case "Fri":console.log("Friday");
+    break;
+  case "Sat":console.log("Saturday");
+    break;
+}
